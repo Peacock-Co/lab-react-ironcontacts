a8674f5cf68f98a89609c9fcd9c6cd59fc321501
Peacock-Co <pavaoleonardo@hotmail.com>
2020-03-10 17:51:07 +0100

    Added component RamdomActor.js

--- starter-code/src/components/RamdomActor.js
+++ starter-code/src/components/RamdomActor.js
@@ -1,8 +1,8 @@
-import React, { useState } from "react";
 import contacts from "../contacts.json";
 
 export const RamdomActor = () => {
   const actor = contacts[Math.floor(Math.random() * contacts.length)];
+  contacts.concat(actor);
 
   console.log(actor);
 };
